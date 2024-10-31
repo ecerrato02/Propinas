698a46917102b3d2813892150e110f49b0e4e2d5
eduar <eduardojosue.cerrato@institutvidreres.cat>
2024-10-31 13:44:13 +0100

    commit reset

--- app/src/main/java/com/ecerrato/propinas/MainActivity.kt
+++ app/src/main/java/com/ecerrato/propinas/MainActivity.kt
@@ -3,9 +3,7 @@ package com.ecerrato.propinas
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Switch
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -29,29 +27,39 @@ class MainActivity : AppCompatActivity() {
 
         val calcButton: Button = binding.calcButton
 
-        val resultadoTexto: TextView = binding.textViewTips
-
         calcButton.setOnClickListener {
-            val form: RadioGroup = binding.radioGroupForm
-            val idSeleccionada: Int = form.checkedRadioButtonId
-            val redondear: Switch = binding.switchRedonear
-            val radioButton: RadioButton = findViewById(idSeleccionada)
-            val numero: TextView = binding.editTextTextCoste
-            val precio = numero.text.toString().toDouble()
 
-            var propina: Double = when (radioButton.text) {
-                "Amazing (20%)" -> precio * 0.20
-                "Good (18%)" -> precio * 0.12
-                "Okay (15%)" -> precio * 0.15
-                else -> 0.0
-            }
 
-            if(redondear.isChecked){
-                propina = Math.round(propina).toDouble()
-                resultadoTexto.setText(propina.toString())
-            }else{
-                val output: String = (Math.round(propina * 10.0) / 10.0).toString()
-                resultadoTexto.text = output
+
+            if(binding.editTextTextCoste.text.toString().isEmpty()){
+                val text = "Debes meter un valor para calcular"
+                val duration = Toast.LENGTH_SHORT
+
+                val toast = Toast.makeText(this, text, duration)
+                toast.show()
+            }else {
+
+                val radioButton: RadioButton = findViewById(binding.radioGroupForm.checkedRadioButtonId)
+                val precio = binding.editTextTextCoste.text.toString().toDouble()
+
+                var propina: Double = when (radioButton.text) {
+                    "Amazing (20%)" -> precio * 0.20
+                    "Good (18%)" -> precio * 0.12
+                    "Okay (15%)" -> precio * 0.15
+                    else -> 0.0
+                }
+
+                if (binding.switchRedonear.isChecked) {
+                    val output: String = "Propina a pagar: " + Math.round(propina).toDouble() + "€."
+                    binding.textViewTips.text = output
+
+
+                } else {
+                    val output: String =
+                        "Propina a pagar: " + (Math.round(propina * 100.0) / 100.0).toString() + "€."
+                    binding.textViewTips.text = output
+
+                }
             }
         }
     }
